8e3e73064c36bbb5e0c9b468a7dbd3b0e7ea7c33
ndh29101 <ndh2912001@gmail.com>
2022-06-07 15:42:45 +0700

    final update

---
+++ _6_6_2022/src/main/java/deserialize.java
@@ -0,0 +1,25 @@
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+import src.main.java.zoo;
+
+import java.io.File;
+import java.io.IOException;
+
+public class deserialize {
+    public static void main(String[] args) throws IOException {
+        DatumReader<zoo> zooDatumReader = new SpecificDatumReader<zoo>(zoo.class);
+
+        DataFileReader<zoo> dataFileReader = new DataFileReader(new File("/home/ndh29101/masterdev/_6_6_2022/src/main/java/data.avro"), zooDatumReader);
+
+        zoo zoos = null;
+        while(dataFileReader.hasNext()){
+
+            zoos =dataFileReader.next(zoos);
+            System.out.println(zoos);
+        }
+    }
+}
