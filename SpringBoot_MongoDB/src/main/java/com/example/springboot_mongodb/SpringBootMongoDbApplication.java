1220ffa535521a9a27e14595953eb37003f945ac
ndh29101 <ndh2912001@gmail.com>
2022-06-24 10:52:49 +0700

    update

--- SpringBoot_MongoDB/src/main/java/com/example/springboot_mongodb/SpringBootMongoDbApplication.java
+++ SpringBoot_MongoDB/src/main/java/com/example/springboot_mongodb/SpringBootMongoDbApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootMongoDbApplication {
 
-    @Autowired
-    private BooksRepository booksRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMongoDbApplication.class, args);
     }
